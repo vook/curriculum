4a3131f6b8f5c74425a8e1f72ff32db142d86f7e
Jhony Mota <jmotaoliveira4@stefanini.com>
2021-01-22 18:38:14 -0300

    Remove css do react-vertical-timeline-component

--- src/components/pages/Career.js
+++ src/components/pages/Career.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core";
-import 'react-vertical-timeline-component/style.min.css';
 import {Page} from "../partials/Page";
 import {useTranslation} from "react-i18next";
 import Timeline from "../partials/Timeline";
